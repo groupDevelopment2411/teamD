6de0bf1baf62f62995b10264282ef0f6ac20ddd1
LAPTOP-G83TG7GA\user <kanuma.thiramith@gmail.com>
2025-01-24 21:11:49 +0900

    Employee記述

---
+++ src/main/java/com/example/demo/search/Employee.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String department;
+    private int age;
+
+    // getters and setters
+}
