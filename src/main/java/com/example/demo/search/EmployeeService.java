6447056105ad6be21764236d1fc2c3f4f7e85686
LAPTOP-G83TG7GA\user <kanuma.thiramith@gmail.com>
2025-02-10 22:34:35 +0900

    EnploeeService修正

--- src/main/java/com/example/demo/search/EmployeeService.java
+++ src/main/java/com/example/demo/search/EmployeeService.java
@@ -1,7 +1,6 @@
 package com.example.demo.search;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,27 +13,13 @@ public class EmployeeService {
         this.repository = repository;
     }
 
-    public List<Employee> searchEmployees(String id, String name, String minAge, String maxAge, String startDate, String endDate) {
-        validateInputs(id, minAge, maxAge, startDate, endDate);
-        Long employeeId = (id != null && !id.isEmpty()) ? Long.parseLong(id) : null;
-        Integer min = (minAge != null && !minAge.isEmpty()) ? Integer.parseInt(minAge) : null;
-        Integer max = (maxAge != null && !maxAge.isEmpty()) ? Integer.parseInt(maxAge) : null;
-        LocalDate start = (startDate != null && !startDate.isEmpty()) ? LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy/MM/dd")) : null;
-        LocalDate end = (endDate != null && !endDate.isEmpty()) ? LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy/MM/dd")) : null;
+    public List<Employee> searchEmployees(String id, String name, String minAge, String maxAge, 
+			LocalDate startDateFrom, LocalDate startDateTo, 
+			LocalDate endDateFrom, LocalDate endDateTo) {
+    	Long employeeId = (id != null && !id.isEmpty()) ? Long.parseLong(id) : null;
+    	Integer min = (minAge != null && !minAge.isEmpty()) ? Integer.parseInt(minAge) : null;
+    	Integer max = (maxAge != null && !maxAge.isEmpty()) ? Integer.parseInt(maxAge) : null;
 
-        return repository.findEmployees(employeeId, name, min, max, start, end);
-    }
-
-    private void validateInputs(String id, String minAge, String maxAge, String startDate, String endDate) {
-        if (id != null && !id.matches("\\d+")) {
-            throw new IllegalArgumentException("");
-        }
-        if ((minAge != null && !minAge.matches("\\d+")) || (maxAge != null && !maxAge.matches("\\d+"))) {
-            throw new IllegalArgumentException("");
-        }
-        if ((startDate != null && !startDate.matches("\\d{4}/\\d{2}/\\d{2}")) ||
-            (endDate != null && !endDate.matches("\\d{4}/\\d{2}/\\d{2}"))) {
-            throw new IllegalArgumentException("");
-        }
-    }
+    	return repository.findEmployees(employeeId, name, min, max, startDateFrom, startDateTo, endDateFrom, endDateTo);
+}
 }
