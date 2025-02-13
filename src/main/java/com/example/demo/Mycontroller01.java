c3056ab39747f4ca34494de567a1637c5d637426
LAPTOP-DA47A4TP\tokud <tokuhataraku@gmail.com>
2025-02-13 17:24:37 +0900

    ログイン画面、IDの送信まで

--- src/main/java/com/example/demo/Mycontroller01.java
+++ src/main/java/com/example/demo/Mycontroller01.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@controller
+@Controller
 public class Mycontroller01 {
-	
-		@RequestMapping("/first")
-		public String first() {
-			return "index";
-		}
-
+	@RequestMapping("/toppage")
+	public String toppage() {
+		return "index";
 	}
+	@PostMapping("/Login")
+	public String Login(
+		Model m,
+		@RequestParam("name") String name,
+		@RequestParam("pass") String pass
+	
+	) {
+		m.addAttribute("name" ,name);
+		m.addAttribute("pass" ,pass);
+		return "Login";
+	}
+	
+	}
+
 
