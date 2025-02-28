98df7761f3e495aa5fb242d185adfa6a875c5d98
LAPTOP-DA47A4TP\tokud <tokuhataraku@gmail.com>
2025-02-28 19:19:39 +0900

    ログイン日時追加

--- src/main/java/com/example/demo/Mycontroller2.java
+++ src/main/java/com/example/demo/Mycontroller2.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Mycontroller2 {
-	@RequestMapping("/Select")
-	public String Select() {
+	@GetMapping("/Select")
+	public String Select(Model model) {
+		LocalDateTime currentTime = LocalDateTime.now();
+		model.addAttribute("currentTime",currentTime);
 		 return"index2";
 	}
 
