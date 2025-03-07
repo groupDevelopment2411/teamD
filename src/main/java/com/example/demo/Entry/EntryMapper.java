b1a35a277c335c82b3a22df63610deb60497ebc3
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-07 20:19:58 +0900

    マッパークラス作成

---
+++ src/main/java/com/example/demo/Entry/EntryMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.Entry;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+//マッパークラス
+@Mapper
+public interface EntryMapper {
+	//データの登録
+	@Insert("insert into users(name, age, password) values(#{name}, #{age}, #{password})")
+	void insert(Entry entry);
+}
