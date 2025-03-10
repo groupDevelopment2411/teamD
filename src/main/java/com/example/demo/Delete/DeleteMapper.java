e3cfcae93e9178fc026b1e707281a91ebccc0e9c
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-10 23:30:20 +0900

    マッパークラスにSQL文追加

--- src/main/java/com/example/demo/Delete/DeleteMapper.java
+++ src/main/java/com/example/demo/Delete/DeleteMapper.java
@@ -1,5 +1,12 @@
 package com.example.demo.Delete;
 
-public interface DeleteMapper {
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
 
+//マッパークラス
+@Mapper
+public interface DeleteMapper {
+	
+	@Delete("delete from users where id = #{id}")
+	void delete(int id);
 }
