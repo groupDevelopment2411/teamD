eaa32d2bef6865e070d92bc0b0c555a6ccb808c3
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-10 23:02:01 +0900

    エンティティクラスに変数、コンストラクタ、ゲッターとセッターを追加

--- src/main/java/com/example/demo/Delete/Delete.java
+++ src/main/java/com/example/demo/Delete/Delete.java
@@ -3,6 +3,20 @@ package com.example.demo.Delete;
 //エンティティクラス
 public class Delete {
 	//変数
+	//社員ID
 	private int id;
 	
+	//コンストラクタ
+	public Delete() {};
+	public Delete(int id) {
+		this.id = id;
+	}
+	
+	//ゲッターとセッター
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
