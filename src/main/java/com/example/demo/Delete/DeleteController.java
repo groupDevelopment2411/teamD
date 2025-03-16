f2b08ec54b0614315d91526b0c655f0bc8ab4454
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-16 22:40:53 +0900

    idを削除できるか確認

--- src/main/java/com/example/demo/Delete/DeleteController.java
+++ src/main/java/com/example/demo/Delete/DeleteController.java
@@ -1,5 +1,32 @@
 package com.example.demo.Delete;
 
-public class DeleteController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+//コントローラークラス
+@Controller
+public class DeleteController {
+	//インスタンス
+	@Autowired
+	private DeleteService service;
+	
+	@RequestMapping("/deleteForm")
+	public String deleteForm() {
+		return "deleteForm";
+	}
+	
+	@PostMapping("/delete")
+	public String delete(
+			Model m,
+			@RequestParam("id") String id
+			) {
+		int numId = Integer.parseInt(id);
+		service.delete(numId);
+		m.addAttribute("msg", "社員情報の削除が完了しました");
+		return "deleteResult";
+	}
 }
