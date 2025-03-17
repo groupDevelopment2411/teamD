8eae2b17aef624f9c8d5bb91d0d3731f5bd34ca1
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-17 23:12:18 +0900

    メニューと検索画面への遷移追加

--- src/main/java/com/example/demo/Delete/DeleteController.java
+++ src/main/java/com/example/demo/Delete/DeleteController.java
@@ -3,6 +3,7 @@ package com.example.demo.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,5 +29,18 @@ public class DeleteController {
 		service.delete(numId);
 		m.addAttribute("msg", "社員情報の削除が完了しました");
 		return "deleteResult";
+	}
+	
+	//社員情報削除(完了)
+	//メニュー画面へ遷移ボタン
+	@GetMapping("/menu")
+	public String menu() {
+		return "menu";
+	}
+		
+	//検索画面へ遷移ボタン
+	@GetMapping("search")
+	public String search() {
+		return "search";
 	}
 }
