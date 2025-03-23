3ff5b5a17ddde4102cdd7f343e064f8d2ed49877
岡本 啓寛 <o.akkie.720@gmail.com>
2025-03-24 00:14:39 +0900

    アクセスがあったURLに戻れるように実装

--- src/main/java/com/example/demo/Delete/DeleteController.java
+++ src/main/java/com/example/demo/Delete/DeleteController.java
@@ -1,5 +1,8 @@
 package com.example.demo.Delete;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,30 +17,46 @@ public class DeleteController {
 	//インスタンス
 	@Autowired
 	private DeleteService service;
-	
+
 	@RequestMapping("/deleteForm")
-	public String deleteForm() {
+	public String deleteForm(HttpSession session, HttpServletRequest request) {
+		String referer = request.getHeader("Referer");
+		if (referer != null) {
+			session.setAttribute("previousUrl", referer);
+		}
 		return "deleteForm";
 	}
-	
+
+	//社員情報登録(入力)の戻るボタンでアクセスのあった画面に遷移
+	@PostMapping("/previous")
+	public String previous(HttpSession session) {
+		// 保存されているURLを取得
+		String previousUrl = (String) session.getAttribute("previousUrl");
+		// URLが存在しない場合はデフォルトでメニュー画面へ
+		if (previousUrl == null || previousUrl.isEmpty()) {
+			return "redirect:/menu";
+		}
+		return "redirect:" + previousUrl;
+	}
+
+	//社員情報削除(確認)
 	@PostMapping("/delete")
 	public String delete(
 			Model m,
-			@RequestParam("id") String id
-			) {
+			@RequestParam("id") String id) {
 		int numId = Integer.parseInt(id);
 		service.delete(numId);
 		m.addAttribute("msg", "社員情報の削除が完了しました");
 		return "deleteResult";
 	}
-	
+
 	//社員情報削除(完了)
 	//メニュー画面へ遷移ボタン
 	@GetMapping("/menu")
 	public String menu() {
 		return "menu";
 	}
-		
+
 	//検索画面へ遷移ボタン
 	@GetMapping("search")
 	public String search() {
